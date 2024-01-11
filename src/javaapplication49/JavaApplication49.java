30ec12dad176a647d84eaf09545af77bffed022d
csc340sp24 <M_GARCIAPAE@uncg.edu>
2024-01-11 16:30:17 -0500

    No

--- src/javaapplication49/JavaApplication49.java
+++ src/javaapplication49/JavaApplication49.java
@@ -14,7 +14,7 @@ public class JavaApplication49 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("No");
     }
     
 }
